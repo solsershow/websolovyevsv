f8e5b17fedf01830292f3ce3a08c849b40492e37
solsershow <160650965+solsershow@users.noreply.github.com>
2024-03-26 22:08:22 +0300

    index.js

--- index.js
+++ index.js
@@ -5,18 +5,19 @@ const fs = require("fs")
 const port = 3000
 
 app.use(express.json())
+app.use(express.urlencoded())
 
 app.get('*', (req, res) => {
-  res.sendFile('reg.html', {root: path.join(__dirname)})
+  res.sendFile("reg.html", {root: path.join(__dirname)})
 })
 
-app.post("/req-data", (req, res) => {
+app.post('/reg-data', (req, res) => {
     console.log(">>>", req.body)
-    fs.appendFile("data.txt", JSON.stringify(req.body), (err) => {
+    fs.appendFile("data.txt", JSON.stringify(req.body) + "\n", (err) => {
         if (err) {
             res.status(500).send("User not added")
         } else {
-            res.status(100).send("User added")
+            res.status(201).send("User added")
         }
     })
 })
